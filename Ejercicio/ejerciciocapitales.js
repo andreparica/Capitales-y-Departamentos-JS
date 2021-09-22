5bb24ef7b9f105757907a7c33c35d6c152d4bdcc
andreparica <geniusparica@gmail.com>
2021-09-21 19:44:07 -0500

    primer commit

---
+++ Ejercicio/ejerciciocapitales.js
@@ -0,0 +1,142 @@
+
+const nombreDepartamento = () => {
+ let departamento = prompt ("Ingresa el nombre de un departamento de Colombia");
+
+ switch (departamento) {
+  case "amazonas":
+    alert("La capital de Amazonas es Leticia");
+    break;
+
+  case "antioquia":
+    alert("La capital de Antioquia es Medellín");
+    break;
+
+  case "arauca":
+    alert("La capital de Arauca es Arauca");
+    break;
+
+  case "bolivar":
+    alert("La capital de Bolívar es Cartagena de Indias");
+    break;
+
+  case "boyaca":
+    alert("La capital de Boyacá es Tunja");
+    break;
+
+  case "caldas":
+    alert("La capital de Caldas es Manizales");
+    break;
+
+  case "caqueta":
+    alert("La capital de Caquetá es Florencia");
+    break;
+
+  case "casanare":
+    alert("La capital de Casanare es Yopal");
+    break;
+
+  case "cauca":
+    alert("La capital de Cauca es Popayán");
+    break;
+
+  case "cesar":
+    alert("La capital de Cesar es Valledupar");
+    break;
+
+  case "choco":
+    alert("La capital de Chocó es Quibdó");
+    break;
+
+  case "cordoba":
+    alert("La capital de Córdoba es Montería");
+    break;
+
+  case "cundinamarca":
+    alert("La capital de Cundinamarca es Bogotá");
+    break;
+
+  case "guainia":
+    alert("La capital de Guainía es Inírida");
+    break;
+
+  case "guaviare":
+    alert("La capital de Guaviare es San José del Guaviare");
+    break;
+
+  case "huila":
+    alert("La capital de Huila es Neiva");
+    break;
+
+  case "la guajira":
+    alert("La capital de La Guajira es Riohacha");
+    break;
+
+  case "magdalena":
+    alert("La capital de Magdalena es Santa Marta");
+    break;
+
+  case "meta":
+    alert("La capital de Meta es Villavicencio");
+    break;
+
+  case "nariño":
+    alert("La capital de Nariño es San Juan de Pasto");
+    break;
+
+  case "norte de santander":
+    alert("La capital del Norte de Santander es San José de Cúcuta");
+    break;
+
+  case "magdalena":
+    alert("La capital de Magdalena es Santa Marta");
+    break;
+
+  case "putumayo":
+    alert("La capital de Putumayo es Mocoa");
+    break;
+
+  case "quindio":
+    alert("La capital de Quindío es Armenia");
+    break;
+
+  case "risaralda":
+    alert("La capital de Risaralda es Pereira");
+    break;
+
+  case "san andres":
+    alert("La capital de San Andrés es San Andrés");
+    break;
+
+  case "santander":
+    alert("La capital de Santander es Bucaramanga");
+    break;
+
+  case "sucre":
+    alert("La capital de Sucre es Sincelejo");
+    break;
+
+  case "tolima":
+    alert("La capital de Tolima es Ibagué");
+    break;
+
+  case "valle del cauca":
+    alert("La capital de Valle del Cauca es Cali");
+    break;
+
+  case "vaupes":
+    alert("La capital de Vaupés es Mitú");
+    break;
+
+  case "vichada":
+    alert("La capital de Vichada es Puerto Carreño");
+    break;
+
+  case "bogota":
+    alert("La capital de Bogotá es Bogotá");
+    break;
+
+  default:
+    alert("Favor, debes ingresar un departamento colombiano, de lo contrario, no será posible conocer su capital");
+    break;
+}
+}
